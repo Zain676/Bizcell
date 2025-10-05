dda57d7c7245661fb5ed45f4dcadd5876098249e
Zain676 <mzainq9@gmail.com>
2025-10-05 21:59:56 +0500

    adding 3 new sections

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+  (function(){
+    const slider = document.querySelector(".testimonial-slider");
+    const slides = document.querySelectorAll(".slide");
+    let index = 0, interval = 4000;
 
-window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) { // 50px down
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
+    function goTo(i){
+      slider.style.transform = `translateX(-${i * 100}%)`;
     }
-});
 
+    function next(){
+      index = (index + 1) % slides.length;
+      goTo(index);
+    }
+
+    let timer = setInterval(next, interval);
+
+    // optional: pause on hover
+    slider.addEventListener('mouseenter', ()=> clearInterval(timer));
+    slider.addEventListener('mouseleave', ()=> timer = setInterval(next, interval));
+  })();
